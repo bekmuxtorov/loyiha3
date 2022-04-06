d4581a9a07b5be90cf846abda245951b1e4f8a07
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2022-04-06 08:07:21 +0500

    tajriba
    tajriba

--- js/sendtelegram.js
+++ js/sendtelegram.js
@@ -2,13 +2,12 @@
 var telegram_bot_id = "5286641890:AAHnyuP4YtPJ1j1R4n_nkIdOKrJlXksEUM8"; // token'ni o'rniga Siz yaratgan Bot tokenini yozing
 //chat id
 var chat_id = 1603330179; // 1111'ni o'rniga habar borishi kerak bo'lgan joyni ID'sini yozing (Batafsil videoda)
-var u_name, email, message, shortText ;
+var u_name, email, message;
 var ready = function() { 
     u_name = document.getElementById("name").value;
-    shortText = document.getElementById("short_text").value;
     email = document.getElementById("email").value;
     message = document.getElementById("message").value;
-    message = "Ismi: " + u_name + "\nEmail: " + email + "\nQisqacha xat: " + shortText +  "\nXabar: " + message;
+    message = "Ismi: " + u_name + "\nEmail: " + email +  "\nXabar: " + message;
 };
 var sendtelegram = function() {
     ready();
@@ -32,6 +31,5 @@ var sendtelegram = function() {
     document.getElementById("name").value = "";
     document.getElementById("email").value = "";
     document.getElementById("message").value = "";
-    document.getElementById("short_text").value = "";
     return false;
 };
