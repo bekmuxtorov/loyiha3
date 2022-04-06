b994b7d76eecf83f239f45e5755472e7498fe51f
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2022-04-06 06:27:38 +0500

    keraksizlari o'chirildi
    keraksizlari o'chirildi

--- js/sendtelegram.js
+++ js/sendtelegram.js
@@ -2,12 +2,12 @@
 var telegram_bot_id = "5151521204:AAGKfwCd-MZ9J74xShC4yvxreyreTmmmSjU"; // token'ni o'rniga Siz yaratgan Bot tokenini yozing
 //chat id
 var chat_id = 1603330179; // 1111'ni o'rniga habar borishi kerak bo'lgan joyni ID'sini yozing (Batafsil videoda)
-var u_name, email, message, shortText, message1;
+var u_name, email, message, shortText, ;
 var ready = function() { 
     u_name = document.getElementById("name").value;
     shortText = document.getElementById("short_text").value;
     email = document.getElementById("email").value;
-    message1 = document.getElementById("message1").value;
+    message = document.getElementById("message").value;
     message = "Ismi: " + u_name + "\nEmail: " + email + "\nQisqacha xat: " + shortText +  "\nXabar: " + message;
 };
 var sendtelegram = function() {
