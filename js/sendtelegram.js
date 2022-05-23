c394f078092a9b37b9be43db07cb00af60e6cb5c
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2022-05-23 23:07:00 +0500

    Bot kanalga ulandi

--- js/sendtelegram.js
+++ js/sendtelegram.js
@@ -1,7 +1,7 @@
 //bot token
 var telegram_bot_id = "5278330498:AAE43NrKG8yEyybosaiD6Y1C69RnqYpRAe4"; // token'ni o'rniga Siz yaratgan Bot tokenini yozing
 //chat id
-var chat_id =1603330179; // 1111'ni o'rniga habar borishi kerak bo'lgan joyni ID'sini yozing (Batafsil videoda)
+var chat_id =-1001632055744; // 1111'ni o'rniga habar borishi kerak bo'lgan joyni ID'sini yozing (Batafsil videoda)
 var u_name, email, message, number;
 var ready = function() {
     u_name = document.getElementById("name").value;
